02c3a1c11a04f06abc7e7d269622d309163519b1
Anurag Bachheti <157354714+Anurag-Bachheti@users.noreply.github.com>
2025-04-19 18:03:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-//your JS code here. If required.
+let block2 = document.getElementById("first2");
+
+const promise1 = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+			resolve("Promise 1")
+		}, 1000);
+    });
+}
+
+const promise2 = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+			resolve("Promise 2")
+		}, 1000);
+    });
+};
+
+const promise3 = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() =>
+			{resolve("Promise 3")
+		}, 1000);
+    });
+};
+
+Promise.all([promise1(), promise2(), promise3()]).then((data) => {
+	document.getElementById("first1").innerHTML = data[0];
+	document.getElementById("first2").innerHTML = data[0].getMiliseconds;
+});
+
+
+
+
+
